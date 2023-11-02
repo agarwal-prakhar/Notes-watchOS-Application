c90cffec492bc7ba6dec54890b746c6219fd52e5
Prakhar Agarwal <realprakhar@outlook.com>
2023-11-02 17:47:01 +0530

    Initial Commit

---
+++ Notes Watch App/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes Watch App
+//
+//  Created by Prakhar Agarwal on 02/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Notes_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
